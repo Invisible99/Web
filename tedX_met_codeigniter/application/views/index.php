e986bbbd146d7fdfb0a6001a6901df2b32a4b2e9
Frederik Thuysbaert <fthuysbaert@gmail.com>
2015-04-29 09:12:30 +0200

    fixen bootstrap implementatie
    fixen in views

---
+++ tedX_met_codeigniter/application/views/index.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="<?= base_url(array('assets', 'bootstrap-3.3.4-dist', 'css', 'bootstrap.min.css')) ?>">
+        <script src="<?base_url(array('assets', 'jquery', 'jquery-2.1.3.min.js'))?>"></script>
+        <title>403 Forbidden</title>
+    </head>
+    <body>
+
+        <p>Directory access is forbidden.</p>
+
+    </body>
+</html>
