3812cd82c5795f4f0b7138910b498134b7439ffc
Jarno Michiels <wefknrise@gmail.com>
2015-05-22 20:43:14 +0200

    Nog meer rommel

--- WebProject/application/controllers/user.php
+++ WebProject/application/controllers/user.php
@@ -2,27 +2,9 @@
 
 class User extends CI_Controller {
 
-    public function __construct() {
-        parent::__construct();
-
-// Load form helper library
-        $this->load->helper('form');
-
-// Load form validation library
-        $this->load->library('form_validation');
-
-// Load session library
-        $this->load->library('session');
-
-// Load database
-        $this->load->model('login_database');
-    }
-
     //Show index page
     function index() {
-        $this->load->view('header');
         $this->load->view('index');
-        $this->load->view('footer');
     }
 
     //Show events page
@@ -38,32 +20,6 @@ class User extends CI_Controller {
     //Show register page
     function register() {
         $this->load->view('index');
-    }
-
-// Validate and store registration data in database
-    public function new_user_registration() {
-
-// Check validation for user input in SignUp form
-        $this->form_validation->set_rules('username', 'Username', 'trim|required|xss_clean');
-        $this->form_validation->set_rules('email', 'Email', 'trim|required|xss_clean');
-        $this->form_validation->set_rules('password', 'Password', 'trim|required|xss_clean');
-        if ($this->form_validation->run() == FALSE) {
-            $this->load->view('register');
-        } else {
-            $data = array(
-                'username' => $this->input->post('username'),
-                'email' => $this->input->post('email'),
-                'password' => $this->input->post('password')
-            );
-            $result = $this->login_database->registration_insert($data);
-            if ($result == TRUE) {
-                $data['message_display'] = 'Registration Successfully !';
-                $this->load->view('login_form', $data);
-            } else {
-                $data['message_display'] = 'Username already exist!';
-                $this->load->view('registration_form', $data);
-            }
-        }
     }
 
 }
