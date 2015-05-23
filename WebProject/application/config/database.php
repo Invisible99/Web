e0e2908bf4f1d9309eef1e68ecd3f6120a4b8358
Piet Vandeput <piet-v@live.com>
2015-05-23 16:01:05 +0200

    Mysqli naar PDO

--- WebProject/application/config/database.php
+++ WebProject/application/config/database.php
@@ -63,12 +63,12 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'	=> '',
+	'dsn'	=> 'mysql:host=localhost;dbname=tedxpxl',
 	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => '',
 	'database' => 'tedxpxl',
-	'dbdriver' => 'mysqli',
+	'dbdriver' => 'pdo',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => TRUE,
