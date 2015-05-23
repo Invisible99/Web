6a55c76e369c5905964324d7f87bf70cd477a780
Stef Janssens <theinvisible99@hotmail.com>
2015-05-23 15:05:32 +0200

    Databank

---
+++ tedxpxl.sql
@@ -0,0 +1,156 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Machine: 127.0.0.1
+-- Gegenereerd op: 23 mei 2015 om 15:01
+-- Serverversie: 5.6.17
+-- PHP-versie: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Databank: `tedxpxl`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `categories`
+--
+
+CREATE TABLE IF NOT EXISTS `categories` (
+  `categorieID` int(11) NOT NULL AUTO_INCREMENT,
+  `titel` varchar(75) NOT NULL,
+  `omschrijving` varchar(255) NOT NULL,
+  PRIMARY KEY (`categorieID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `categories`
+--
+
+INSERT INTO `categories` (`categorieID`, `titel`, `omschrijving`) VALUES
+(1, 'Evenementen', 'Hier komen alle geplande en voorbije evenementen.'),
+(2, 'Gasten forum', 'forum voor de gasten'),
+(3, 'Leden forum', 'forum voor de leden'),
+(4, 'Nieuws', 'Al het nieuws van TedxPxl');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `posts`
+--
+
+CREATE TABLE IF NOT EXISTS `posts` (
+  `berichtID` int(11) NOT NULL AUTO_INCREMENT,
+  `topicID` int(11) NOT NULL,
+  `gebruikerID` int(11) NOT NULL,
+  `bericht` text NOT NULL,
+  `postDate` date NOT NULL,
+  PRIMARY KEY (`berichtID`),
+  KEY `gebruikerID` (`gebruikerID`),
+  KEY `topicID` (`topicID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `roles`
+--
+
+CREATE TABLE IF NOT EXISTS `roles` (
+  `rolID` int(11) NOT NULL AUTO_INCREMENT,
+  `rolNaam` varchar(75) NOT NULL,
+  PRIMARY KEY (`rolID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `roles`
+--
+
+INSERT INTO `roles` (`rolID`, `rolNaam`) VALUES
+(1, 'admin'),
+(2, 'lid'),
+(3, 'niet-lid'),
+(4, 'Verwijderd'),
+(5, 'Banned');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `threads`
+--
+
+CREATE TABLE IF NOT EXISTS `threads` (
+  `topicID` int(11) NOT NULL AUTO_INCREMENT,
+  `titel` varchar(75) NOT NULL,
+  `bericht` text NOT NULL,
+  `gebruikerID` int(11) NOT NULL COMMENT 'gebruiker die deze topic heeft aangemaakt',
+  `categorieID` int(11) NOT NULL,
+  PRIMARY KEY (`topicID`),
+  KEY `gebruikerID` (`gebruikerID`),
+  KEY `categorieID` (`categorieID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `gebruikerID` int(11) NOT NULL AUTO_INCREMENT,
+  `rolID` int(11) NOT NULL DEFAULT '11',
+  `username` varchar(75) NOT NULL COMMENT 'Ding wat gegenereerd wordt bij aanvraag nieuwe user',
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `profielfoto` mediumblob,
+  PRIMARY KEY (`gebruikerID`),
+  UNIQUE KEY `email` (`email`),
+  KEY `rolID` (`rolID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `users`
+--
+
+INSERT INTO `users` (`gebruikerID`, `rolID`, `username`, `password`, `email`, `profielfoto`) VALUES
+(1, 1, 'admin.admin', 'pxl', 'admin.admin@admin.be', NULL),
+(2, 2, 'lid.lid', 'pxl', 'lid.lid@lid.be', NULL),
+(3, 3, 'guest.guest', 'pxl', 'guest.guest@guest.be', NULL);
+
+--
+-- Beperkingen voor geëxporteerde tabellen
+--
+
+--
+-- Beperkingen voor tabel `posts`
+--
+ALTER TABLE `posts`
+  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`topicID`) REFERENCES `threads` (`topicID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `posts_ibfk_3` FOREIGN KEY (`gebruikerID`) REFERENCES `users` (`gebruikerID`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Beperkingen voor tabel `threads`
+--
+ALTER TABLE `threads`
+  ADD CONSTRAINT `threads_ibfk_2` FOREIGN KEY (`categorieID`) REFERENCES `categories` (`categorieID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `threads_ibfk_1` FOREIGN KEY (`gebruikerID`) REFERENCES `users` (`gebruikerID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Beperkingen voor tabel `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`rolID`) REFERENCES `roles` (`rolID`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
