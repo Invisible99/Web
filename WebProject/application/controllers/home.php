7791f2c1d1570bf1c5a008cd65baccc1530a200f
Piet Vandeput <piet-v@live.com>
2015-05-23 18:04:01 +0200

    Home categorie weggedaan

--- WebProject/application/controllers/home.php
+++ WebProject/application/controllers/home.php
@@ -4,21 +4,21 @@ class Home extends CI_Controller {
 
     //Show index page
     function index() {
-        $this->load->view('home/index');
+        $this->load->view('index');
     }
 
     //Show events page
     function events() {
-        $this->load->view('home/events');
+        $this->load->view('events');
     }
 
     //Show register page
     function register() {
-        $this->load->view('home/register');
+        $this->load->view('register');
     }
     
     //Show contact page
     function contact() {
-        $this->load->view('home/contact');
+        $this->load->view('contact');
     }
 }
