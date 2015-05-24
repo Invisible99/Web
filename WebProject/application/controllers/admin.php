38b9d07a17d5b92a4cc75a0b0da5400a95f6922f
khownz <koen895@hotmail.com>
2015-05-24 19:41:24 +0200

    overzicht gebruikers + functies voor activeren, verwijderen, bannen, unbannen, deactiveren
    MOET NOG GEBEUREN: bewerken (profielfoto en username),
    
    controle of inloggen nog werkt zoals het moet
    
    zorgen dat de admin dropdown, inclusief het aantal inactieve gebruikers
    ENKEL getoond wordt aan een admin

--- WebProject/application/controllers/admin.php
+++ WebProject/application/controllers/admin.php
@@ -5,6 +5,15 @@ class Admin extends CI_Controller {
     function __construct() {
         parent::__construct();
         $this->load->model('users_model');
+
+        $this->load->library('session');
+        $this->load->library('user_agent');
+        $this->load->helper('url');
+
+        if (!($this->session->has_userdata('user') && $this->session->has_userdata('logged_in') && $this->session->logged_in && $this->session->has_userdata('rolID') && $this->session->userdata['rolID'] == 1)) {
+            $this->session->sess_destroy();
+            redirect(base_url() . "login/index");
+        }
     }
 
     //Show overzichtsplatvorm page
@@ -13,31 +22,65 @@ class Admin extends CI_Controller {
     }
 
     function gebruikerOverzicht() {
-        $this->load->library('session');
-        $this->load->library('user_agent');
-        $this->load->helper('url');
-        //hier controle of amdin wel admin is --> deze controle moet eignelijk gebeuren VOOR je in deze method kan geraken, 
-        //wnt nrml mag je de dropdown niet zien dan!, ma  toch nog hier ergens zodat als ze de url rechtstreeks ingeven dat ze hier nog niet geraken!!!!
-        
-        if($this->session->has_userdata('user') && $this->session->has_userdata('logged_in') && $this->session->logged_in && $this->session->has_userdata('rolID')){
-            
-            $this->data['melding'] = "";
-            $this->data['gebruikers'] = $this->users_model->findall();
-            if (empty($this->data['gebruikers'])) {
-                $this->data['melding'] = "<div class='alert alert-danger'>Er zijn geen records in de tabel users.</div>";
-            }
-            $this->parser->parse('admin/overzicht_gebruikers.php', $this->data); //op de vieuw zelf nog controleren of hij wel admin is? kan je rechtstreeks met de url naar die pagina?
+        $this->data['actievemelding'] = "";
+        $this->data['inactievemelding'] = "";
+        $this->data['bannedmelding'] = "";     
+        $this->data['aantalInactieveGebruikers'] = 0;
+
+        $this->data['actieveGebruikers'] = $this->users_model->findallActive();     
+        if (empty($this->data['actieveGebruikers'])) {
+            $this->data['actievemelding'] .= "<div class='alert alert-info'>Er zijn geen geactiveerde gebruikers.</div>";
         }
-        else{
-            redirect(base_url() ."login/index");
+
+        $this->data['inactieveGebruikers'] = $this->users_model->findallInActive();
+        $this->data['aantalInactieveGebruikers'] = count($this->data['inactieveGebruikers']);
+        if (empty($this->data['inactieveGebruikers'])) {
+            $this->data['inactievemelding'] .= "<div class='alert alert-info'>Er zijn geen gebruikers om te activeren.</div>";
         }
-        
+
+        $this->data['bannedGebruikers'] = $this->users_model->findallBanned();
+        if (empty($this->data['bannedGebruikers'])) {
+            $this->data['bannedmelding'] .= "<div class='alert alert-info'>Er zijn geen banned gebruikers.</div>";
+        }
+        $this->parser->parse('admin/overzicht_gebruikers.php', $this->data); //op de vieuw zelf nog controleren of hij wel admin is? kan je rechtstreeks met de url naar die pagina?
+    }
+    
+    /*function geefInactiveUserCount(){
+        $this->data['inactieveGebruikers'] = $this->users_model->findallInActive();
+    }*/
+
+    //enkel gebruiken voor nog niet geactiveerde gebruikers, deze hebben toch nog niets anders kunnen doen in de database dus die mogen echt verwijderd worden, anderen zal een update moeten gebeuren naar "removed user"
+    function deleteGebruiker($gebruikerID) {
+        $this->users_model->deleteID(array('gebruikerID' => $gebruikerID));
+        redirect(base_url() . "admin/gebruikerOverzicht");
     }
 
     function activeerGebruiker($gebruikerID) {
-        
+        $this->users_model->updateID(array('actief' => 1), array('gebruikerID' => $gebruikerID));
+        redirect(base_url() . "admin/gebruikerOverzicht");
     }
-    
+
+    function unbanGebruiker($gebruikerID) {
+        $this->users_model->updateID(array('rolID' => 2), array('gebruikerID' => $gebruikerID));
+        redirect(base_url() . "admin/gebruikerOverzicht");
+    }
+
+    function deactivateGebruiker($gebruikerID) {
+        $this->users_model->updateID(array('rolID' => 4, 'username' => 'Removed User', 'email' => $gebruikerID . '@noemail.com', 'actief' => 0), array('gebruikerID' => $gebruikerID));
+        redirect(base_url() . "admin/gebruikerOverzicht");
+    }
+
+    function banGebruiker($gebruikerID) {
+        $this->users_model->updateID(array('rolID' => 5), array('gebruikerID' => $gebruikerID));
+        redirect(base_url() . "admin/gebruikerOverzicht");
+    }
+
+    function bewerkGebruiker($gebruikerID) {
+        //$this->users_model->deleteID(array('gebruikerID' => $gebruikerID));
+        print_r($gebruikerID);
+        //redirect(base_url() . "admin/gebruikerOverzicht");
+    }
+
     function evenementOverzicht() {
         
     }
